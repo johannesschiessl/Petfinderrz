ce1301c54c1eab0a5922d7550373ff019cf38c85
Johannes Schießl <ignotumnomen@icloud.com>
2025-01-09 18:01:55 +0100

    feat(landing): add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-green-600 text-white py-6">
+      <div className="container mx-auto px-6 text-center">
+        <p>&copy; 2025 Petfinderrz. Alle Rechte vorbehalten.</p>
+      </div>
+    </footer>
+  );
+}
