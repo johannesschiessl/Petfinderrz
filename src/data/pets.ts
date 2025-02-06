e50587fb0852bcc1115016610828bf5fffd1e2d2
Jakob Grätz <96118933+jakobgraetz@users.noreply.github.com>
2025-02-06 14:48:34 +0100

    chore: add image sources to pets.ts (#3)
    * chore: add image source 1 to data/pets.ts
    
    * chore: add image source 2 to data/pets.ts
    
    * chore: add image source 3 to data/pets.ts
    
    * chore: add image source 4 to data/pets.ts
    
    * chore: add image source 5 to data/pets.ts
    
    * chore: add image source 6 to data/pets.ts
    
    * chore: add image source 7 to data/pets.ts
    
    * chore: add image source 8 to data/pets.ts
    
    * chore: add image source 9 to data/pets.ts
    
    * chore: add image source 10 to data/pets.ts

--- src/data/pets.ts
+++ src/data/pets.ts
@@ -6,7 +6,7 @@ export const pets: Pet[] = [
     name: "Flauschi",
     race: "Perserkatze",
     category: "Katze",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fHww"],
     location: {
       distance: 12.5,
       city: "Augsburg",
@@ -18,7 +18,7 @@ export const pets: Pet[] = [
     name: "Bello",
     race: "Golden Retriever",
     category: "Hund",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1612774412771-005ed8e861d2?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8R29sZGVuJTIwUmV0cmlldmVyfGVufDB8fDB8fHww"],
     location: {
       distance: 20.3,
       city: "Ingolstadt",
@@ -30,7 +30,7 @@ export const pets: Pet[] = [
     name: "Schnurri",
     race: "Siamkatze",
     category: "Katze",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1568152950566-c1bf43f4ab28?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2lhbWVzZSUyMGNhdHxlbnwwfHwwfHx8MA%3D%3D"],
     location: {
       distance: 35.8,
       city: "Regensburg",
@@ -42,7 +42,7 @@ export const pets: Pet[] = [
     name: "Rex",
     race: "Deutscher Schäferhund",
     category: "Hund",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1605725657590-b2cf0d31b1a5?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8R2VybWFuJTIwc2hlcGhlcmR8ZW58MHx8MHx8fDA%3D"],
     location: {
       distance: 50.0,
       city: "Nürnberg",
@@ -54,7 +54,7 @@ export const pets: Pet[] = [
     name: "Luna",
     race: "Maine Coon",
     category: "Katze",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1611236628716-ae1a7e98591b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fE1haW5lJTIwQ29vbnxlbnwwfHwwfHx8MA%3D%3D"],
     location: {
       distance: 10.4,
       city: "Freising",
@@ -66,7 +66,7 @@ export const pets: Pet[] = [
     name: "Hopsi",
     race: "Zwergkaninchen",
     category: "Kleintier",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVubnl8ZW58MHx8MHx8fDA%3D"],
     location: {
       distance: 25.6,
       city: "Rosenheim",
@@ -78,7 +78,7 @@ export const pets: Pet[] = [
     name: "Kiki",
     race: "Wellensittich",
     category: "Vogel",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1594911588637-c9e891bd7858?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YnVkZ2llfGVufDB8fDB8fHww"],
     location: {
       distance: 40.7,
       city: "Passau",
@@ -90,7 +90,7 @@ export const pets: Pet[] = [
     name: "Strolchi",
     race: "Dackel",
     category: "Hund",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1618265341355-d0e2d1fdf26b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGFjaHNodW5kfGVufDB8fDB8fHww"],
     location: {
       distance: 18.2,
       city: "Landsberg am Lech",
@@ -102,7 +102,7 @@ export const pets: Pet[] = [
     name: "Moppel",
     race: "Meerschweinchen",
     category: "Kleintier",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1533152162573-93ad94eb20f6?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z3VpbmVhJTIwcGlnfGVufDB8fDB8fHww"],
     location: {
       distance: 30.3,
       city: "Bayreuth",
@@ -114,7 +114,7 @@ export const pets: Pet[] = [
     name: "Rocky",
     race: "Border Collie",
     category: "Hund",
-    images: ["/placeholder.svg?height=300&width=300"],
+    images: ["https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Qm9yZGVyJTIwQ29sbGllfGVufDB8fDB8fHww"],
     location: {
       distance: 45.9,
       city: "Würzburg",
